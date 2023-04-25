00e04900dbe4eafde74a38b3929f66de3485f1d2
ioncelciuc <ioncelciuc2001@gmail.com>
2023-04-25 05:57:09 +0300

    add: card prices click transfers user to website

--- Frontend/flutter_app/lib/screens/card_details/card_prices_ui.dart
+++ Frontend/flutter_app/lib/screens/card_details/card_prices_ui.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/models/card_prices.dart';
 import 'package:flutter_app/models/yugioh_card.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class CardPricesUi extends StatelessWidget {
   final YuGiOhCard card;
@@ -18,126 +19,100 @@ class CardPricesUi extends StatelessWidget {
         children: [
           Card(
             margin: const EdgeInsets.fromLTRB(8, 8, 8, 0),
-            child: Container(
-              padding: const EdgeInsets.all(8),
-              child: Row(
-                children: [
-                  Image.asset(
-                    'assets/cardmarket.png',
-                    width: 200,
-                    height: 50,
+            child: InkWell(
+              onTap: () {
+                launchUrl(
+                  Uri.parse(
+                    'https://www.cardmarket.com/en/YuGiOh/Products/Singles?searchString=${card.name!}',
                   ),
-                  Expanded(
-                    child: Container(
-                      alignment: Alignment.centerRight,
-                      margin: const EdgeInsets.only(right: 8),
-                      child: Text(
-                        '${card.cardPrices!.cardmarketPrice}\$',
-                        style: const TextStyle(fontSize: 15),
+                );
+              },
+              child: Container(
+                height: 70,
+                padding: const EdgeInsets.all(8),
+                child: Row(
+                  children: [
+                    Image.asset(
+                      'assets/cardmarket.png',
+                    ),
+                    Expanded(
+                      child: Container(
+                        alignment: Alignment.centerRight,
+                        margin: const EdgeInsets.fromLTRB(16, 8, 16, 8),
+                        child: Text(
+                          '${card.cardPrices!.cardmarketPrice}\$',
+                          style: const TextStyle(fontSize: 15),
+                        ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ),
           Card(
             margin: const EdgeInsets.fromLTRB(8, 8, 8, 0),
-            child: Container(
-              padding: const EdgeInsets.all(8),
-              child: Row(
-                children: [
-                  Image.asset(
-                    'assets/tcgplayer.jpg',
-                    width: 200,
-                    height: 50,
+            child: InkWell(
+              onTap: () {
+                launchUrl(
+                  Uri.parse(
+                    'https://www.tcgplayer.com/search/yugioh/product?productLineName=yugioh&q=${card.name!}&view=grid&ProductTypeName=Cards&page=1',
                   ),
-                  Expanded(
-                    child: Container(
-                      alignment: Alignment.centerRight,
-                      margin: const EdgeInsets.only(right: 8),
-                      child: Text(
-                        '${card.cardPrices!.tcgplayerPrice}\$',
-                        style: const TextStyle(fontSize: 15),
+                );
+              },
+              child: Container(
+                height: 70,
+                padding: const EdgeInsets.fromLTRB(16, 8, 16, 8),
+                child: Row(
+                  children: [
+                    Image.asset(
+                      'assets/tcgplayer.png',
+                    ),
+                    Expanded(
+                      child: Container(
+                        alignment: Alignment.centerRight,
+                        margin: const EdgeInsets.only(right: 8),
+                        child: Text(
+                          '${card.cardPrices!.tcgplayerPrice}\$',
+                          style: const TextStyle(fontSize: 15),
+                        ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ),
           Card(
             margin: const EdgeInsets.fromLTRB(8, 8, 8, 0),
-            child: Container(
-              padding: const EdgeInsets.all(8),
-              child: Row(
-                children: [
-                  Image.asset(
-                    'assets/amazon.png',
-                    width: 200,
-                    height: 50,
+            child: InkWell(
+              onTap: () {
+                launchUrl(
+                  Uri.parse(
+                    'https://www.coolstuffinc.com/main_search.php?pa=searchOnName&page=1&resultsPerPage=25&q=${card.name!}&sh=1',
                   ),
-                  Expanded(
-                    child: Container(
-                      alignment: Alignment.centerRight,
-                      margin: const EdgeInsets.only(right: 8),
-                      child: Text(
-                        '${card.cardPrices!.amazonPrice}\$',
-                        style: const TextStyle(fontSize: 15),
+                );
+              },
+              child: Container(
+                padding: const EdgeInsets.fromLTRB(16, 16, 16, 16),
+                height: 70,
+                child: Row(
+                  children: [
+                    Image.asset(
+                      'assets/coolstuffinc.png',
+                    ),
+                    Expanded(
+                      child: Container(
+                        alignment: Alignment.centerRight,
+                        margin: const EdgeInsets.only(right: 8),
+                        child: Text(
+                          '${card.cardPrices!.coolstuffincPrice}\$',
+                          style: const TextStyle(fontSize: 15),
+                        ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          Card(
-            margin: const EdgeInsets.fromLTRB(8, 8, 8, 0),
-            child: Container(
-              padding: const EdgeInsets.all(8),
-              child: Row(
-                children: [
-                  Image.asset(
-                    'assets/ebay.png',
-                    width: 200,
-                    height: 50,
-                  ),
-                  Expanded(
-                    child: Container(
-                      alignment: Alignment.centerRight,
-                      margin: const EdgeInsets.only(right: 8),
-                      child: Text(
-                        '${card.cardPrices!.ebayPrice}\$',
-                        style: const TextStyle(fontSize: 15),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          Card(
-            margin: const EdgeInsets.fromLTRB(8, 8, 8, 0),
-            child: Container(
-              padding: const EdgeInsets.all(8),
-              child: Row(
-                children: [
-                  Image.asset(
-                    'assets/coolstuffinc.png',
-                    width: 200,
-                    height: 50,
-                  ),
-                  Expanded(
-                    child: Container(
-                      alignment: Alignment.centerRight,
-                      margin: const EdgeInsets.only(right: 8),
-                      child: Text(
-                        '${card.cardPrices!.coolstuffincPrice}\$',
-                        style: const TextStyle(fontSize: 15),
-                      ),
-                    ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ),
