bd721b08745cb98043659c277395f45a5803479b
ioncelciuc <ioncelciuc2001@gmail.com>
2023-05-08 22:39:48 +0300

    add: game rules & begginer guide download links to dashboard

--- Frontend/flutter_app/lib/screens/dashboard/dashboard_ui.dart
+++ Frontend/flutter_app/lib/screens/dashboard/dashboard_ui.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/components/app_bar_drawer.dart';
+import 'package:flutter_app/components/custom_button.dart';
 import 'package:flutter_app/utils/deck_search_delegate.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class DashboardUi extends StatelessWidget {
   final GlobalKey<ScaffoldState> homeScaffoldState;
@@ -27,8 +29,8 @@ class DashboardUi extends StatelessWidget {
       ),
       body: ListView(
         padding: const EdgeInsets.symmetric(vertical: 32, horizontal: 32),
-        children: const [
-          Text(
+        children: [
+          const Text(
             'Welcome, Duelist!',
             style: TextStyle(
               fontSize: 30,
@@ -36,7 +38,30 @@ class DashboardUi extends StatelessWidget {
             ),
             textAlign: TextAlign.center,
           ),
-          SizedBox(height: 32),
+          const SizedBox(height: 32),
+          CustomButton(
+            title: 'Download Game Rules',
+            onPressed: () async {
+              await launchUrl(
+                Uri.parse(
+                  'https://img.yugioh-card.com/en/downloads/rulebook/SD_RuleBook_EN_10.pdf',
+                ),
+                mode: LaunchMode.externalApplication,
+              );
+            },
+          ),
+          const SizedBox(height: 32),
+          CustomButton(
+            title: 'Download Begginer\'s Guide',
+            onPressed: () async {
+              await launchUrl(
+                Uri.parse(
+                  'https://img.yugioh-card.com/en/downloads/rulebook/YS17_BG_EN.pdf',
+                ),
+                mode: LaunchMode.externalApplication,
+              );
+            },
+          ),
         ],
       ),
     );
